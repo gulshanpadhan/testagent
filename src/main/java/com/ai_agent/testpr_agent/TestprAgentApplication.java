facdeaba9cffe0f1facb3cb41e2db0d3953eb936
fg_jaynesh <jaynesh.ahir@futuregroup.in>
2025-03-13 11:32:24 +0530

    initial push

---
+++ src/main/java/com/ai_agent/testpr_agent/TestprAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ai_agent.testpr_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprAgentApplication.class, args);
+	}
+
+}
